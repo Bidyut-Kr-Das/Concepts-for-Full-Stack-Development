bbfdc5607f2ff2678ad59e2f5dde973c800ce35f
bidyut-kr-das <bkdas2017.bd@gmail.com>
2025-11-14 16:53:27 +0530

    [INTI] React project

---
+++ first-react-project/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <section className="flex gap-2 justify-center flex-row flex-wrap h-screen bg-slate-400">
+      <div className="h-12 aspect-square rounded-full bg-red-500"></div>
+      <div className="h-12 aspect-square rounded-full bg-blue-500"></div>
+      <div className="h-12 aspect-square rounded-full bg-emerald-500"></div>
+      <div className="h-12 aspect-square rounded-full bg-amber-500"></div>
+      <div className="h-12 aspect-square rounded-full bg-violet-500"></div>
+      <div className="h-12 aspect-square rounded-full bg-purple-500"></div>
+    </section>
+  );
+}
+
+export default App;
